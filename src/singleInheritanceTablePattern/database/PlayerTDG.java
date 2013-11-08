dc2ed9ec43bfce1daa4ea652ea3d341ad500d0ad
Harry Zhang <haiyuezhang@hotmail.com>
2013-11-08 15:02:02 -0500

    joe's code using common db

--- src/singleInheritanceTablePattern/database/PlayerTDG.java
+++ src/singleInheritanceTablePattern/database/PlayerTDG.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import common.database.*;
 
 public class PlayerTDG {
 	public static ResultSet findAllPlayers() throws SQLException{
